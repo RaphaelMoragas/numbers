0a8737c93bc0682942a297a47cd8675a317f7305
Raphael <raphael6384@gmail.com>
2022-12-10 20:37:05 +0000

    Test Spock

--- src/test/java/ListAggregatorTest.java
+++ src/test/java/ListAggregatorTest.java
@@ -17,7 +17,6 @@ public class ListAggregatorTest {
     @Test
     public void sum() {
 
-
         ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
 
@@ -27,7 +26,6 @@ public class ListAggregatorTest {
     @Test
     public void max() {
 
-
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
 
@@ -36,7 +34,6 @@ public class ListAggregatorTest {
 
     @Test
     public void max1() {
-
         list = Arrays.asList(1,2,4,2);
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
@@ -79,13 +76,9 @@ public class ListAggregatorTest {
     @Test
     public void distinct() {
 
-
         ListAggregator aggregator = new ListAggregator();
-
         GenericListDeduplicator deduplicator =  Mockito.mock(GenericListDeduplicator.class);
         Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
-
-
         int distinct = aggregator.distinct(list, deduplicator);
         Assertions.assertEquals(3, distinct);
 
