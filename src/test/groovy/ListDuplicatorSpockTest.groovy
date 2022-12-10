a9b1c6eb6a7dad5aeb933eb2a56d5105467ae483
Raphael <raphael6384@gmail.com>
2022-12-10 20:44:56 +0000

    Spock Sorter List

---
+++ src/test/groovy/ListDuplicatorSpockTest.groovy
@@ -0,0 +1,20 @@
+import spock.lang.Specification
+
+class ListDuplicatorSpockTest extends Specification{
+    private def list
+
+    def setup() {
+        list = Arrays.asList(1,2,4,2,5)
+    }
+
+    def 'test duplicator'(){
+        given:
+        def sorter = Mock(GenericListSorter)
+        sorter.sort(_) >> Arrays.asList(1, 2, 2, 4, 5)
+        def duplicator = new ListDeduplicator(sorter)
+        when:
+        def unic = duplicator.deduplicate(list)
+        then:
+        unic == [1,2,4,5]
+    }
+}
---
+++ src/test/groovy/ListSorterSpockTest.groovy
@@ -0,0 +1,17 @@
+import spock.lang.Specification
+
+class ListSorterSpockTest extends Specification{
+    def private list
+
+    def setup() {
+        list = Arrays.asList(1,2,4,2,5)
+    }
+    def 'test sorter list'(){
+        given:
+        def sorter  = new ListSorter()
+        when:
+        def r = sorter.sort(list)
+        then:
+        r == [1,2,2,4,5]
+    }
+}
